9ff70781f13e7372c3a85192149c4f5fc95fb9ee
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-10-18 10:49:16 +0300

    Create JwtResponse.java

---
+++ auth-service/src/main/java/com/batuhan/project/dto/JwtResponse.java
@@ -0,0 +1,12 @@
+package com.batuhan.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String email;
+}
