eca29b71bbb186210f1009bdeef800d49ffff9bf
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-10-25 11:06:02 +0300

    Create ConfirmationTokenRepository.java

---
+++ notification-service/src/main/java/com/batuhan/project/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.batuhan.project.repository;
+
+import com.batuhan.project.entity.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+}
