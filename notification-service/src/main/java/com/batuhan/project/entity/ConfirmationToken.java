51e2feb528a595045968feadc1dacc56d713a4af
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-10-25 11:05:09 +0300

    Create ConfirmationToken.java

---
+++ notification-service/src/main/java/com/batuhan/project/entity/ConfirmationToken.java
@@ -0,0 +1,41 @@
+package com.batuhan.project.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "confirmation_token")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @Column(nullable = false)
+    private String userEmail;
+
+    public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, String userEmail) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.userEmail = userEmail;
+    }
+}
