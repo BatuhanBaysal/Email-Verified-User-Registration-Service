3ed6040d5e4454ffbfd043706353a2e3fbe20459
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-10-26 12:00:32 +0300

    Update EmailConfirmationListener.java

--- notification-service/src/main/java/com/batuhan/project/service/EmailConfirmationListener.java
+++ notification-service/src/main/java/com/batuhan/project/service/EmailConfirmationListener.java
@@ -3,6 +3,8 @@ package com.batuhan.project.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -11,14 +13,30 @@ import com.batuhan.project.event.UserRegisteredEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-
 @Component
+@RequiredArgsConstructor
 public class EmailConfirmationListener {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ConfirmationTokenService tokenService;
+
+    @PostConstruct
+    public void setup() {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     public void handleMessage(String message) {
         System.out.println("-------------------------------------");
-        System.out.println("NOTIFICATION SERVICE:");
-        System.out.println("New Message Received (String): " + message);
+        System.out.println("NOTIFICATION SERVICE: Starting processing JSON...");
+
+        try {
+            UserRegisteredEvent event = objectMapper.readValue(message, UserRegisteredEvent.class);
+            System.out.println("Message Successfully Parsed (POJO). Email: " + event.getEmail());
+            tokenService.createAndSaveToken(event.getEmail());
+
+        } catch (JsonProcessingException e) {
+            System.err.println("ERROR: Failed to parse incoming JSON message: " + e.getMessage());
+        }
         System.out.println("-------------------------------------");
     }
 }
